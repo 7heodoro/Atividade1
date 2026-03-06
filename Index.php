1805e627a8f672e5572fccecd9a1d857c9338364
Theodoro <kaiohenrique1979@gmail.com>
2026-03-06 20:17:11 -0300

    Aula 5

---
+++ Index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <main>
+        
+    <header class="titulo">
+        <h1>E-Sports News</h1>
+    </header>
+
+<nav class="titulo"> 
+                <ul>
+                    <li><a href ="cs2.html">Counter Strike 2</a></li>
+                    <li><a href="valorant.html">Valorant</a></li>
+                    <li><a href="lol.html">League of Legends</a></li>
+                </ul>
+                
+            </nav>
+
+
+    
+            <section class="bloquinho">
+                <h1>Counter Strike 2</h1>
+                    <h2>Seis equipes lutam por uma vaga na PGL Bucharest 2026</h2>
+                    <a href="cs2.html">
+                <img src="Imagens/imagem1.jpg">
+                        </a>
+                    <p>A classificatória da América do Sul para PGL Bucharest 2026 começa nesta quinta-feira (26) e vai até domingo (1º). Oito times vão brigar por uma vaga no evento principal, sendo que Fluxo W7M abriu mão da disputa para focar na BetBoom RUSH B! Summit S2.</p>
+                    <p>Por conta disso, a Vivo Keyd Stars herdou a vaga do Fluxo. Galoyrs, FOLHA AMARELA, Yawara e Bounty Hunters foram as melhores colocadas das seletivas. Imperial, MIBR e ShindeN receberam os convites via ranking da Valve.</p>
+
+                    <ul>
+                        <li>BR Imperial</li>
+                        <li>BR MIBR</li>
+                        <li>AR ShindeN</li>
+                        <li>BR Vivo Keyd Stars</li>
+                        <li>BR Galorys</li>
+                        <li>BR FOLHA AMARELA</li>
+                        <li>BR Yawara</li>
+                        <li>BR Bounty Hunters</li>
+                    </ul>
+                </section>
+
+
+
+            <section class="bloquinho">
+                <h1>Valorant</h1>    
+                    <h2>ArtziN assume liderança na FURIA e fala sobre exigência no Masters Santiago: “Quero que se divirtam”</h2>      
+                    <a href="valorant.html">
+                <img src="Imagens/imagem2.jpg">     
+                    </a>
+                <p>Na tarde desta sexta-feira (27), Arthur “artziN” Araujo foi um dos representantes da FURIA na coletiva de imprensa que dá início ao VALORANT Masters Santiago 2026. Único jogador brasileiro na equipe titular da Pantera, o capitão falou sobre seu papel de liderança no time e como sua experiência pode agregar durante a campanha no torneio internacional a ser disputado no Chile.</p>
+            </section>
+
+
+
+            <section class="bloquinho">
+                <h1>League of legends</h1>     
+                <h2>“Varus foi buffado demais”, diz rioter sobre mudanças no Patch 26.05</h2>   
+            <a href="lol.html">
+            <img src="Imagens/imagem3.webp">
+            </a>
+            
+                <p>A Riot já detalhou quais mudanças devem chegar na próxima atualização do League of Legends. O rioter Phroxzon detalhou em seu perfil no X (ex-Twitter) os fortalecimentos para a Lilia e os nerfs para Varus, além de divulgar o que a desenvolvedora pretende alterar em 12 campeões e dois itens.</p>
+            </section>
+            
+<footer class="bloquinho">
+    <p><?php echo date('Y'); ?></p>
+</footer>
+
+    </main>
+</body>
+
+
+</html>
